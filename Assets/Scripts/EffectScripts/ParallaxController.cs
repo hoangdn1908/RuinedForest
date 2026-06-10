cea2cfe38008d22b472087220e7840abcdbf1adb
Hoangdn1908 <hoangtranb02@gmail.com>
2026-06-10 14:10:59 +0700

    Create parallax effect for background

---
+++ Assets/Scripts/EffectScripts/ParallaxController.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class ParallaxController : MonoBehaviour
+{
+    private Transform cam;
+    private Vector3 camStartPos;
+    private float distance;
+    private GameObject[] backgrounds;
+    private Material[] materials;
+    private float[] backSpeed;
+    private float farthestBack;
+    [Range(0.01f, 0.05f)]
+    public float parallaxSpeed;
+
+    private void Start()
+    {
+        cam = Camera.main.transform;
+        camStartPos = cam.position;
+        int backCount = transform.childCount;
+        materials = new Material[backCount];
+        backSpeed = new float[backCount];
+        backgrounds = new GameObject[backCount];
+
+        for (int i = 0; i < backCount; i++) 
+        {
+            backgrounds[i] = transform.GetChild(i).gameObject;
+            materials[i] = backgrounds[i].GetComponent<Renderer>().material;
+        }
+        BackSpeedCalculate(backCount);
+    }
+
+    private void BackSpeedCalculate(int backCount) 
+    {
+        for (int i = 0; i < backCount; i++) 
+        {
+            if (backgrounds[i].transform.position.z - cam.position.z > farthestBack)
+                farthestBack = backgrounds[i].transform.position.z - cam.position.z;
+        }
+
+        for (int i = 0; i < backCount; i++) 
+        {
+            backSpeed[i] = 1 - (backgrounds[i].transform.position.z - cam.position.z);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(cam.position.x, transform.position.y, 0);
+        distance = cam.position.x - camStartPos.x;
+        for (int i = 0; i < backgrounds.Length; i++) 
+        {
+            float speed = backSpeed[i] * parallaxSpeed;
+            materials[i].SetTextureOffset("_MainTex",new Vector2(distance, 0) * speed);
+        }
+    }
+}
