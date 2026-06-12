878b432ffefe988e0924193a1708629b4d7002bc
Hoangdn1908 <hoangtranb02@gmail.com>
2026-06-12 15:20:56 +0700

    Update damage by using playerdata

--- Assets/Scripts/PlayerScripts/Hit_HurtBox/PlayerHitBox.cs
+++ Assets/Scripts/PlayerScripts/Hit_HurtBox/PlayerHitBox.cs
@@ -2,20 +2,27 @@ using UnityEngine;
 
 public class PlayerHitBox : MonoBehaviour
 {
-    private float damage = 10f;
+    [SerializeField] private PlayerController playerController;
+    private float damage;
 
     private void Awake()
     {
-
+        playerController = GetComponentInParent<PlayerController>();
+        SetDamage();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Enemy")) 
+        if (collision.CompareTag("Enemy"))
         {
             EnemyHealth enemyHealth = collision.GetComponentInParent<EnemyHealth>();
             if (enemyHealth == null) return;
             enemyHealth.TakeDamage(damage);
             Debug.Log(damage);
         }
+    }
+
+    private void SetDamage() 
+    {
+        damage = playerController.PlayerData.attackDamage;
     }
 }
