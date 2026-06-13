77d6c06e5bb021112d994943c60b9be2f0bec2af
Hoangdn1908 <hoangtranb02@gmail.com>
2026-06-13 14:05:59 +0700

    Update PlayerHitBox.cs

--- Assets/Scripts/PlayerScripts/Hit_HurtBox/PlayerHitBox.cs
+++ Assets/Scripts/PlayerScripts/Hit_HurtBox/PlayerHitBox.cs
@@ -17,7 +17,6 @@ public class PlayerHitBox : MonoBehaviour
             EnemyHealth enemyHealth = collision.GetComponentInParent<EnemyHealth>();
             if (enemyHealth == null) return;
             enemyHealth.TakeDamage(damage);
-            Debug.Log(damage);
         }
     }
 
