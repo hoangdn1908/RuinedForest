3d2b018faefa064a01a7bae0b4141d5612fb1e87
Hoangdn1908 <hoangtranb02@gmail.com>
2026-06-13 16:04:51 +0700

    Update code

--- Assets/Scripts/Enemy/EnemyStates/AttackState/EnemyAttackState.cs
+++ Assets/Scripts/Enemy/EnemyStates/AttackState/EnemyAttackState.cs
@@ -2,8 +2,63 @@ using UnityEngine;
 
 public class EnemyAttackState : EnemyBaseState
 {
+    private float attackTimer;
    public EnemyAttackState(EnemyController enemyController, EnemyStateMachine enemyStateMachine) : base(enemyController, enemyStateMachine)
     {
 
+    }
+
+    public override void Enter()
+    {
+        StopMovement();
+        PlayAttackAnimation();
+        SetAttackTimer();
+    }
+
+    public override void LogicUpdate()
+    {
+        CheckDeathState();
+        AttackPlayer();
+    }
+
+    private void PlayAttackAnimation()
+    {
+        enemyController.enemyAnimation.SetStateAnimation(EnemyAnimationStates.Attack);
+    }
+
+    private void StopMovement()
+    {
+        enemyController.enemyMovement.Stop();
+    }
+    private void SetAttackTimer()
+    {
+        attackTimer = enemyController.EnemyData.attackDuration;
+    }
+
+    private void DecideNextState()
+    {
+        if (enemyController.enemyDetection.CanAttackPlayer(enemyController.EnemyData.attackRange)) SetAttackTimer();
+        else if (!enemyController.enemyDetection.CanAttackPlayer(enemyController.EnemyData.attackRange) && enemyController.EnemyData.canChase)
+            enemyStateMachine.ChangeState(enemyController.enemyChaseState);
+        else enemyStateMachine.ChangeState(enemyController.enemyIdleState);
+    }
+
+    private void AttackPlayer()
+    {
+        attackTimer -= Time.deltaTime;
+        if (attackTimer <= 0)
+        {
+            DecideNextState();
+        }
+    }
+
+    private bool CheckDeathState()
+    {
+        if (!enemyController.enemyHealth.IsAlive())
+        {
+            enemyStateMachine.ChangeState(enemyController.enemyDeathState);
+            return true;
+        }
+        return false;
     }
 }
--- Assets/Scripts/Enemy/EnemyStates/AttackState/EnemyMeleeAttackState.cs
+++ Assets/Scripts/Enemy/EnemyStates/AttackState/EnemyMeleeAttackState.cs
@@ -2,68 +2,13 @@ using UnityEngine;
 
 public class EnemyMeleeAttackState : EnemyAttackState
 {
-    private float attackTimer;
     public EnemyMeleeAttackState(EnemyController enemyController, EnemyStateMachine enemyStateMachine) : base(enemyController, enemyStateMachine)
     {
 
     }
 
-    public override void Enter()
-    {
-        StopMovement();
-        PlayAttackAnimation();
-        SetAttackTimer();
-    }
-
-    public override void LogicUpdate()
-    {
-        CheckDeathState();
-        AttackPlayer();
-    }
-
     public override void Exit()
     {
-        //enemyController.enemyCombat.EndAttack();
-    }
-
-    private void PlayAttackAnimation() 
-    {
-        enemyController.enemyAnimation.SetStateAnimation(EnemyAnimationStates.Attack);
-    }
-
-    private void StopMovement() 
-    {
-        enemyController.enemyMovement.Stop();
-    }
-    private void SetAttackTimer() 
-    {
-        attackTimer = enemyController.EnemyData.attackDuration;
-    }
-
-    private void DecideNextState() 
-    {
-        if (enemyController.enemyDetection.CanAttackPlayer(enemyController.EnemyData.attackRange)) SetAttackTimer();
-        else if (!enemyController.enemyDetection.CanAttackPlayer(enemyController.EnemyData.attackRange) && enemyController.EnemyData.canChase)
-            enemyStateMachine.ChangeState(enemyController.enemyChaseState);
-        else enemyStateMachine.ChangeState(enemyController.enemyIdleState);
-    }
-
-    private void AttackPlayer() 
-    {
-        attackTimer -= Time.deltaTime;
-        if (attackTimer <= 0)
-        {
-            DecideNextState();
-        }
-    }
-
-    private bool CheckDeathState()
-    {
-        if (!enemyController.enemyHealth.IsAlive())
-        {
-            enemyStateMachine.ChangeState(enemyController.enemyDeathState);
-            return true;
-        }
-        return false;
+        enemyController.enemyCombat.EndAttack();
     }
 }
