2c747dcc7d4db33e5186e19194598823c773df22
Hoangdn1908 <hoangtranb02@gmail.com>
2026-06-16 15:33:04 +0700

    Add  spike trap

---
+++ Assets/Scripts/TrapScripts/SpikeController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SpikeController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) 
+        {
+            PlayerHealth playerHealth = collision.GetComponentInParent<PlayerHealth>();
+            if (playerHealth == null) 
+            {
+                Debug.Log("Null");
+                return;
+            }
+            playerHealth.TakeDamage(100);
+        }
+    }
+}
